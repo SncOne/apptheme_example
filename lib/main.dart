df0365c5ffd9f061a8754182713b28c04c228ddd
SncOne <38573319+SncOne@users.noreply.github.com>
2022-02-14 19:41:28 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,11 @@
 import 'package:apptheme_example/theme_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'home_page.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
-}
-
-Future<void> init() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
-  await Hive.openBox('app');
 }
 
 class MyApp extends StatelessWidget {
